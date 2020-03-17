dfc1b390cf69b301988bd2849127dc41389205d4
Mark <markhappy200@gmail.com>
2020-03-17 19:36:32 +0200

    Alpha version 0.2 - getting links now works - changed scrapping method - improved stability, decreased speed

--- bf4Requests.py
+++ bf4Requests.py
@@ -11,122 +11,92 @@ def getNr(currentURL):
     soup = BeautifulSoup(page.content, 'html.parser')
 
     pagesnr = soup.find_all(class_ = "btn btn--muted btn--s")
-    converted_pagesnr = int(pagesnr[(len(pagesnr) - 1)].get_text())
+    if len(pagesnr) == 0:
+        return 0
+    else:
+        converted_pagesnr = int(pagesnr[(len(pagesnr) - 1)].get_text())
     return converted_pagesnr
 
 
-# ================== get car data ==================
-def carsPerPage(currentURL):
-    time.sleep(1)
+# ================== get car links ==================
+def getCarLinks(currentURL):
     headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
     page = requests.get(currentURL, headers = headers)
     soup = BeautifulSoup(page.content, 'html.parser')
-
-    carLink = []
-    for link in soup.find_all('a', {'class': 'link--muted no--text--decoration'}):
-        try:
-            carLink.append(link['href'])
-        except: 
-            None
+    # _classPrivateSeller - cBox-body cBox-body--resultitem fsboAd rbt-reg rbt-no-top
+    # _classDealer - cBox-body--resultitem dealerAd rbt-reg rbt-no-top
+    carLinks = []
     for link in soup.find_all('a', {'class': 'link--muted no--text--decoration result-item'}):
         try:
-            carLink.append(link['href'])
+            carLinks.append(link['href'])
         except: 
             None
-    for link in soup.find_all('a', {'class': 'link--muted no--text--decoration result-item'}):
-        try:
-            carLink.append(link['href'])
-        except: 
-            print("huinea")
-    carTitles = soup.find_all(class_ = "h3 u-text-break-word")
-    carPrices = soup.find_all(class_ = "h3 u-block")
-    carRegDistPow = soup.find_all(class_ = "rbt-regMilPow")
 
-    # ================== filtrating data
-    for i in range(len(carTitles)):
-        carTitles[i] = carTitles[i].get_text()
-        carPrices[i] = carPrices[i].get_text()
-        carRegDistPow[i] = carRegDistPow[i].get_text()
-        
-    # ================== calculating temporary data
-    # car registration temp data
-    i = 0
-    j = 0
-    carReg = []
-    carRegTemp = []
-    for i in range(len(carTitles)):
-        tempstr = ''
-        for j in range(10):
-            tempstr += carRegDistPow[i][j]
-        carRegTemp.append(tempstr)
+    return carLinks
 
-    # car dist temp data
-    i = 0
-    carDist = []
-    carDistTemp = []
-    carDistIndexes = []
-    for i in range(len(carRegDistPow)):
-        tp = 0
-        tp = carRegDistPow[i].find(",", tp)
-        carDistIndexes.append(tp + len(", "))
-        tp = 0
-        tp = carRegDistPow[i].find("km", tp)
-        carDistIndexes.append(tp - len(" "))
 
-    i = 0
-    chk = []
-    for i in range(len(carRegDistPow)):
-        chk.append(carRegDistPow[i])
-        chk.append("4")
+# ================== get car data ==================
+def getCarData(carLinkCurrentPage):
+    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+    page = requests.get(carLinkCurrentPage, headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-    i = 0
-    j = 0
-    tempstr = ""
-    while i != len(chk):
-        tempstr = ""
-        for j in range(carDistIndexes[i], carDistIndexes[i + 1]):
-            tempstr += (chk[i][j])
-        carDist.append(tempstr)
-        i += 2
+    # title
+    carTitle = soup.find(id = "rbt-ad-title").get_text()
 
-    # ================== formatting data for suitable type
-    # car price
-    carPrice = [sub[ : -2] for sub in carPrices]
-    carPrice = [sub.replace('.', '') for sub in carPrice]
-    carPrice = [int(i) for i in carPrice]
+    # price
+    carPrice = soup.find(class_ = "h3 rbt-prime-price").get_text()
 
-    # car registration
-    carReg = [sub[6 : ] for sub in carRegTemp] 
-    for i, en in enumerate(carReg):
-        if en == "en, ":
-            carReg[i] = "2020"
-        if en == "ulas":
-            carReg[i] = "4444"
-    carReg = [int(i) for i in carReg]
+    # registration
+    try:
+        carReg = soup.find(id = "rbt-firstRegistration-v").get_text()
+    except:
+        carReg = soup.find(id = "rbt-category-v").get_text()
+    # mileage
+    try:
+        carMiles = soup.find(id = "rbt-mileage-v").get_text()
+    except:
+        carMiles = 1414
 
-    # car dist
-    carDist = [sub.replace('.', '') for sub in carDist]
-    for i, en in enumerate(carDist):
-        if en == '':
-            carDist[i] = '1414'
-    carDist = [int(i) for i in carDist]
+    # ================== format necessary data
+    # car price first
+    carPrice = carPrice.replace('.', '')
+    if 'Brutto' in carPrice:
+        carPrice = carPrice[ : -11]
+    else:
+        carPrice = carPrice[ : -2]
+    carPrice = int(carPrice)
+
+    # registration
+    if 'Neufahrzeug' in carReg:
+        carReg = 2020
+    elif 'Vorführfahrzeug' in carReg:
+        carReg = 4444
+    elif 'Jahreswagen' in carReg:
+        carReg = 3333
+        #Jahreswagen - employee car
+    else:
+        carReg = carReg[3 : ]
+        carReg = int(carReg)
+
+    # mileage
+    if carMiles != 1414:
+        carMiles = carMiles[ : -3]
+        carMiles = carMiles.replace('.', '')
+        carMiles = int(carMiles)
 
     # ================== neat print all values
-    for i in range(len(carTitles)):
-        print(carLink[i])
+    print(carTitle)
+    print(carPrice, "€")
+    if carReg == 4444:
+        print("Demo Car")
+    elif carReg == 3333:
+        print("Employee Car")
+    else:
+        print(carReg)
+    if carMiles == 1414:
+        print("Either not specified or 0 km\n")
+    else:
+        print(carMiles, "km\n")
 
-        print(carTitles[i])
-
-        print(carPrice[i], " €")
-
-        if carReg[i] == 4444:
-            print("Demo Car")
-        else:
-            print(carReg[i])
-
-        if carDist[i] == 1414:
-            print("Either not specified or 0\n")
-        else:
-            print(carDist[i], " km\n")
-
-    return carLink, carTitles, carPrice, carReg, carDist
+    return carTitle, carPrice, carReg, carMiles
--- mobile.de_Bot.py
+++ mobile.de_Bot.py
@@ -10,14 +10,25 @@ startSearcher(dv)
 firstSearch(dv, carMake, carModel)
 
 currentURL = curURL(dv)
-getNr(currentURL)
+converted_pagesnr = getNr(currentURL)
 
-for currentPage in range(getNr(currentURL)):
+carLink  = []
+carTitle = []
+carPrice = []
+carReg   = []
+carMiles = []
+
+for currentPage in range(converted_pagesnr):
     currentURL = curURL(dv)
-    carsPerPage(currentURL)
+    carLinksCurrentPage = getCarLinks(currentURL)
+    for i in range(len(carLinksCurrentPage)):
+        tmp = getCarData(carLinksCurrentPage[i])
+        carLink.append(carLinksCurrentPage[i])
+        carTitle.append(tmp[0])
+        carPrice.append(tmp[1])
+        carReg.append(tmp[2])
+        carMiles.append(tmp[3])
     nextPage(dv, currentURL, currentPage)
-    print("\n\n=== results for page ", currentPage + 1, "are above _|\n\n")
-    if currentPage == getNr(currentURL) - 1:
-        break
+    print("===== results for page ", currentPage + 1, "are above ↑\n\n")
 
 killd(dv)
