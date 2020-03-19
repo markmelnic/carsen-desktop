c57aa928b1dc0e57c04f7f6fcafca8d6af9dd8c4
Mark <markhappy200@gmail.com>
2020-03-19 20:02:42 +0200

    Alpha version 0.4.1
    - added car engine power reading (horsepower)

--- mobile.de_Bot.py
+++ mobile.de_Bot.py
@@ -12,12 +12,8 @@ import threading
 def getData(threadNumber, carLink, csvWriter):
     time_started = time.time()
     print(threadNumber, "started at", time_started)
-    tmp = getCarData(carLink)
-    carTitle = tmp[0]
-    carPrice = tmp[1]
-    carReg = tmp[2]
-    carMiles = tmp[3]
-    csvWriter.writerow([carLink ,carReg, carTitle, carPrice, carMiles])
+    data = getCarData(carLink)
+    csvWriter.writerow([carLink , data[0], data[1], data[2], data[3], data[4]])
     print(threadNumber, "executed in", time.time() - time_started)
         
 
@@ -70,10 +66,9 @@ def main():
 
     with io.open('cars.csv', 'w', encoding="utf-8", newline='') as csvFile:
         csvWriter = csv.writer(csvFile)
-        csvWriter.writerow(["Ad Link", "Reg. Year", "Title", "Price", "Mileage"])
+        csvWriter.writerow(["Ad Link", "Reg. Year", "Title", "Price", "Mileage", "Power (HP)"])
         threads = []
         for i in range(len(carLink)):
-            #time.sleep(0.4)
             threadNumber = "Thread " + str(i)
             thread = threading.Thread(target = getData, args = (threadNumber, carLink[i], csvWriter))
             threads.append(thread)
@@ -82,7 +77,6 @@ def main():
                 for thread in threads:
                     thread.join()
                 threads = []
-
         for thread in threads:
             thread.join()
         print("All threads are finished")
