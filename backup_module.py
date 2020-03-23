fd4fd8e2246ec7d15594556b2de6984bd02e6045
Mark <markhappy200@gmail.com>
2020-03-23 18:10:47 +0200

    Alpha version 0.3.5
    - new folder structure
    - new backup option
    - back up files in case site fails to answer during execution

---
+++ backup_module.py
@@ -0,0 +1,45 @@
+
+import os
+import time
+import shutil
+import datetime
+
+
+def backup():
+    #print("\n\n\n/====================================\\")
+    print("\nBackup initiated")
+    time.sleep(2)
+    cwd = os.getcwd()
+    os.chdir('./csv files')
+
+    # check for files to be backed up
+    with open("csvFilesIndex.txt", mode="r") as cFi:
+        files = cFi.readlines()
+        cFi.close()
+    print(len(files), "files found")
+
+    # change working directory and get the new one
+    os.chdir(cwd)
+    os.chdir('./backup')
+    date = datetime.datetime.now()
+    date = str(date)
+    date = date.replace(':', '.')
+    os.mkdir(str(date))
+
+    if len(files) == 0:
+        print("Nothing to backup")
+        print("\====================================/\n\n")
+        return
+    else:
+        os.chdir(cwd)
+        os.chdir('./csv files')
+        path = "../backup/" + date
+        backup = shutil.copy("csvFilesIndex.txt", path)
+        for file in files:
+            file = file.strip("\n")
+            backup = shutil.copy(file, path)
+
+    os.chdir(cwd)
+    time.sleep(2)
+    print("Backed up successfully\n")
+    #print("\====================================/\n\n")
