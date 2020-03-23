040e2aaa2ded7c997e291d28b6cd87492adb072a
Mark <markhappy200@gmail.com>
2020-03-23 13:12:59 +0200

    Alpha version 0.6.2
    - introduced a somewhat user interface
    - checker module now shows changes
    - remover module checks whether there are any files to remove first

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import keyboard
+
 from search_module import *
 from checker_module import *
 from remover_module import *
@@ -6,20 +8,30 @@ from remover_module import *
 
 # main function
 if __name__ == '__main__':
-    #time_started = time.time()
-    #print("\n\nTotal execution time = ", time.time() - time_started)
-    print("\n\n==================================")
-    print("Type 1 for initiating a new search")
-    print("     2 for checking existing ones")
-    print("     3 for removing a search")
-    #print("     4 for stopping the program")
-    inp = input("Input: ")
-    prompter = int(inp)
-    if prompter == 1:
-        search()
-    elif prompter == 2:
-        checker()
-    elif prompter == 3:
-        remover()
-    else:
-        print("Incorrect input")
+    while True:
+        #time_started = time.time()
+        #print("\n\nTotal execution time = ", time.time() - time_started)
+        print("\n\n==================================")
+        print("Press 1 for initiating a new search")
+        print("      2 for checking existing ones")
+        print("      3 for removing a search")
+        print("      X to exit")
+        #print("     4 for stopping the program")
+        while True:
+            # searcher
+            if keyboard.is_pressed('F1'):
+                search()
+                break
+            # checker
+            if keyboard.is_pressed('F2'):
+                checker()
+                break
+            # remover
+            if keyboard.is_pressed('F3'):
+                remover()
+                break
+            '''
+            # exit
+            if keyboard.is_pressed('x'):
+                break
+            '''
